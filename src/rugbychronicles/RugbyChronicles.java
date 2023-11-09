4c26d0408264605f64c2c94b666e86c73ed42640
PinkyBabe <piodos.jam22@gmail.com>
2023-11-09 16:45:47 +0800

    Summon Taloy

--- src/rugbychronicles/RugbyChronicles.java
+++ src/rugbychronicles/RugbyChronicles.java
@@ -6,6 +6,12 @@ public class RugbyChronicles {
     
     public static void main(String[] args) {
        Player p = new Player();
+       
+        p.skill1();
+        p.skill1();
+        p.skill1();
+        p.skill3();
+        p.skill2();
     }
     
 }
